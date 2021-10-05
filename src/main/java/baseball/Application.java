a05edeffb09732276b0fd4b8efc018569ae50409
khg <gusrn160304@gmail.com>
2021-10-06 08:20:13 +0900

    fix(Application): Application 모듈 수정
    숫자야구 프로그램 실행가능 하도록
    Application 모듈 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,9 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO 숫자 야구 게임 구현
+    	BaseballManager baseballManager = new BaseballManager();
+    	do {
+    		baseballManager.playGame();
+    	} while (baseballManager.isReplayGame());
     }
 }
